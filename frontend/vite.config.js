19a88143b3b33df6d7162f21dcc0b82f3530e28e
gilbertmunuotz <officialgilbert24@gmail.com>
2024-04-20 20:01:31 +0300

    App configurations

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true, // Required for CORS
+        // secure: false, // If your backend uses HTTPS, set to true
+      },
+    },
+  },
 })
